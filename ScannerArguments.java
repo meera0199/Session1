ef9023d028a5ba9be012585dfd261e0a1b210d33
Meera Surampudi <meera0199@Meeras-MacBook-Air.local>
2017-05-28 13:10:44 -0400

    added scanners

---
+++ ScannerArguments.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ScannerArguments{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String i = sc.nextLine(); 
+		System.out.println(i); 
+	}
+}
